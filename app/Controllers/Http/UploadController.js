bb7b2bcc90fafc6eb116e8e7cf595d782bc6f15d
Simon Carr <simonjcarr@gmail.com>
2020-09-18 14:21:56 +0100

    initial commit

---
+++ app/Controllers/Http/UploadController.js
@@ -0,0 +1,32 @@
+'use strict'
+const Helpers = use("Helpers");
+class UploadController {
+  async index({ view }) {
+    return view.render("upload_files")
+  }
+
+  async upload({ request, view }) {
+    const upload_file = request.file("upload_file", {
+      types: [
+        "zip",
+        "x-7z-compressed",
+        "csv",
+        "vnd.openxmlformats-officedocument.wordprocessingml.document",
+      ],
+      size: "100mb",
+    });
+
+    await upload_file.move(Helpers.publicPath('uploads'), {
+      overwrite: true
+    })
+
+    if (!upload_file.moved()) {
+      console.log(file.error())
+      return view.render('upload_files', {status: 'Error: ' + upload_file.error()})
+    }
+
+    return view.render('upload_files', {status: 'File Uploaded'})
+  }
+}
+
+module.exports = UploadController
